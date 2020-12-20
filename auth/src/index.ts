b2bef37ae8f1b54e82c48055ff2ee1ae367b8652
lordpangan <lord.pangan@gmail.com>
2020-12-20 22:42:56 +0800

    Add routers to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
