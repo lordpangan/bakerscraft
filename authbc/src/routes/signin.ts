ad211605cc68e415aec4fefe142ba411a700588a
lordpangan <lord.pangan@gmail.com>
2021-01-08 00:07:50 +0800

    Add validate-request.ts forgot to add next() on the last part, Add current user handler, sigin and signout functionality

--- authbc/src/routes/signin.ts
+++ authbc/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -15,7 +19,38 @@ router.post(
       .withMessage('You must supply a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invliad credentials');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invliad credentials');
+    }
+
+    // genererate JWT
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it on session object
+    req.session = {
+      jwt: userJWT,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signInRouter };
