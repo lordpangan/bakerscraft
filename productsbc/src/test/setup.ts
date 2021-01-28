770b62a0ad51d3b052d41492c8224bf48524f955
lordpangan <lord.pangan@gmail.com>
2021-01-28 21:12:04 +0800

    Edit productsbc/src/test/setup.ts signin function to use jwt

--- productsbc/src/test/setup.ts
+++ productsbc/src/test/setup.ts
@@ -1,12 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -37,16 +36,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  // Build a JWT payload. {id,email}
+  const payload = {
+    id: '123edsadsad',
+    email: 'test@test.com',
+  };
 
-  const response = await request(app).post('/api/users/signup').send({
-    email,
-    password,
-  });
+  // Create JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get('Set-Cookie');
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
 
-  return cookie;
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // TakeJSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookiee with the encoded data
+  return [`express:sess=${base64}`];
 };
