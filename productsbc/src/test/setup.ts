91eaa4f6d3a7547c089cafabc834e2a75bb9248f
lordpangan <lord.pangan@gmail.com>
2021-01-29 21:46:14 +0800

    Create a separate login for customer and admin for test setup

--- productsbc/src/test/setup.ts
+++ productsbc/src/test/setup.ts
@@ -5,7 +5,8 @@ import jwt from 'jsonwebtoken';
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): string[];
+      signinAdmin(): string[];
+      signinCust(): string[];
     }
   }
 }
@@ -36,11 +37,36 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signinAdmin = () => {
   // Build a JWT payload. {id,email}
   const payload = {
     id: '123edsadsad',
     email: 'test@test.com',
+    auth: 'admin',
+  };
+
+  // Create JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // TakeJSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookiee with the encoded data
+  return [`express:sess=${base64}`];
+};
+
+global.signinCust = () => {
+  // Build a JWT payload. {id,email}
+  const payload = {
+    id: '123edsadsad',
+    email: 'test@test.com',
+    auth: 'customer',
   };
 
   // Create JWT!
