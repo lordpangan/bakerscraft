8c1170f7f49feb31d64925740ebd511aaa0d526f
lordpangan <lord.pangan@gmail.com>
2021-02-24 22:16:55 +0800

    Add order and product models

---
+++ ordersbc/src/models/order.ts
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@lordpangan/common';
+import { ProductDoc } from './product';
+
+export { OrderStatus };
+
+interface ProductOrderDoc {
+  product: ProductDoc;
+  quantity: number;
+}
+
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  products: ProductOrderDoc[];
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  products: ProductOrderDoc[];
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderedProductSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    products: [orderedProductSchema],
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+export { Order };
