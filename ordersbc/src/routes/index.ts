9f400dd308445eb3dc29386a15c52608647b7342
lordpangan <lord.pangan@gmail.com>
2021-02-25 22:18:26 +0800

    Edit ordersbc/src/routes/index.ts to show all orders related to a customer

--- ordersbc/src/routes/index.ts
+++ ordersbc/src/routes/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@lordpangan/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate({ path: 'products', populate: [{ path: 'productId' }] });
+
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
