8a31ea3c0ad7dd074072a673db12ecc51c772314
lordpangan <lord.pangan@gmail.com>
2021-03-05 20:14:12 +0800

    Add the version and mongoose-update-if-current to ordesbc/src/models/product.ts

--- ordersbc/src/models/product.ts
+++ ordersbc/src/models/product.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface ProductAttrs {
   id: string;
@@ -11,6 +12,7 @@ export interface ProductDoc extends mongoose.Document {
   title: string;
   price: number;
   quantity: number;
+  version: number;
 }
 
 interface ProductModel extends mongoose.Model<ProductDoc> {
@@ -43,6 +45,9 @@ const productSchema = new mongoose.Schema(
     },
   }
 );
+
+productSchema.set('versionKey', 'version');
+productSchema.plugin(updateIfCurrentPlugin);
 
 productSchema.statics.build = (attrs: ProductAttrs) => {
   return new Product({
