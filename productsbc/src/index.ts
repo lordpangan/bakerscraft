12a2d3172658e17ac13d48aa68e401ef17ae7e4b
lordpangan <lord.pangan@gmail.com>
2021-04-11 18:24:19 +0800

    Instantiate listeners - not included from th eprevious commit

--- productsbc/src/index.ts
+++ productsbc/src/index.ts
@@ -41,6 +41,7 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close());
 
     new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
