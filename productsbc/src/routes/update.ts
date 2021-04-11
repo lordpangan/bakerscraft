81b29287aa22f9992d757c23f9a1f45a0f5987a0
lordpangan <lord.pangan@gmail.com>
2021-04-11 15:41:48 +0800

    Add orderid to ProductUpdatedPublisher

--- productsbc/src/routes/update.ts
+++ productsbc/src/routes/update.ts
@@ -47,6 +47,7 @@ router.put(
       quantity: product.quantity,
       userId: product.userId,
       version: product.version,
+      orderId: product.orderId,
     });
 
     res.send({ product });
