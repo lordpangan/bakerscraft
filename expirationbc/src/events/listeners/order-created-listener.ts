a071a9ef27dee321c9537ff82883937bef3e4cab
lordpangan <lord.pangan@gmail.com>
2021-04-14 19:47:12 +0800

    Add expirationbc service

---
+++ expirationbc/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,17 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@lordpangan/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queues';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.orderId,
+    });
+
+    msg.ack();
+  }
+}
