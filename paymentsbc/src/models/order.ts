5530e12ca5b70e820a33add6851fcc7adac7d846
lordpangan <lord.pangan@gmail.com>
2021-05-03 21:50:38 +0800

    Edit order.ts to make some corrections on the schema on paymentsbc

--- paymentsbc/src/models/order.ts
+++ paymentsbc/src/models/order.ts
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@lordpangan/common';
+
+interface ProductOrderDoc {
+  productId: string;
+  price: number;
+  quantity: number;
+}
 
 interface OrderAttrs {
   id: string;
   version: number;
   userId: string;
-  price: number;
+  products: ProductOrderDoc[];
   status: OrderStatus;
   paymentRef: string;
   paymentRes?: string;
@@ -14,7 +21,7 @@ interface OrderAttrs {
 interface OrderDoc extends mongoose.Document {
   version: number;
   userId: string;
-  price: number;
+  products: ProductOrderDoc[];
   status: OrderStatus;
   paymentRef: string;
 }
@@ -23,16 +30,29 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
   build(attrs: OrderAttrs): OrderDoc;
 }
 
+const orderedProductSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
 const orderSchema = new mongoose.Schema(
   {
     userId: {
       type: String,
       required: true,
     },
-    price: {
-      type: Number,
-      required: TextTrackCue,
-    },
+    products: [orderedProductSchema],
     status: {
       type: String,
       required: true,
@@ -55,11 +75,14 @@ const orderSchema = new mongoose.Schema(
   }
 );
 
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
     _id: attrs.id,
     version: attrs.version,
-    price: attrs.version,
+    products: attrs.products,
     userId: attrs.userId,
     status: attrs.status,
     paymentRef: attrs.paymentRef,
