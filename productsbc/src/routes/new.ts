504d5cda8405b9e4680bec71072fd7b9053aabe9
lordpangan <lord.pangan@gmail.com>
2021-05-10 21:40:30 +0800

    Fix typo on error messages

--- productsbc/src/routes/new.ts
+++ productsbc/src/routes/new.ts
@@ -19,10 +19,10 @@ router.post(
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price')
       .isFloat({ gt: 0 })
-      .withMessage('Price must be greater thank 0'),
+      .withMessage('Price must be greater than 0'),
     body('quantity')
       .isInt({ gt: -1 })
-      .withMessage('Quantity must be greater thank 0'),
+      .withMessage('Quantity must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
